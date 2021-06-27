add82dc0a00b8e3fadac7fbc2452f36ff3f8c59b
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-06-27 11:32:10 +0900

    db

--- scripts/I00db/db.mk
+++ scripts/I00db/db.mk
@@ -5,8 +5,8 @@ include ../common.mk
 
 OK : $(db)
 
-$(db) : $(db_dir)/hosts.csv $(db_dir)/users.csv
+$(db) : $(host_users_dir)/hosts.csv $(host_users_dir)/users.csv
 	rm -f $(db)
-	echo -n | sqlite3 -separator , -cmd ".import $(db_dir)/hosts.csv hosts" $(db).bak
-	echo -n | sqlite3 -separator , -cmd ".import $(db_dir)/users.csv users" $(db).bak
+	echo -n | sqlite3 -separator , -cmd ".import $(host_users_dir)/hosts.csv hosts" $(db).bak
+	echo -n | sqlite3 -separator , -cmd ".import $(host_users_dir)/users.csv users" $(db).bak
 	mv $(db).bak $(db)
