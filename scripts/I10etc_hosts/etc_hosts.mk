ace0d907548d9d56b4c890fc10f4b3b6213f453a
tau@eidos.ic.i.u-tokyo.ac.jp <tau@logos.t.u-tokyo.ac.jp>
2021-06-27 11:15:56 +0900

    hosts

--- scripts/I10etc_hosts/etc_hosts.mk
+++ scripts/I10etc_hosts/etc_hosts.mk
@@ -8,5 +8,6 @@ OK : /etc/hosts
 
 /etc/hosts : $(db)
 	sqlite3 -separator " " $(db) 'select ip_addr,group_concat(hostname, " ") from hosts group by ip_addr' > hosts
-	$(kv_merge) /etc/hosts hosts > new_etc_hosts
-	$(inst) new_etc_hosts /etc/hosts
+	$(kv_merge) /etc/hosts hosts > etc_hosts.0
+	$(kv_merge) etc_hosts.0 hosts_to_fix_npm > etc_hosts.1
+	$(inst) etc_hosts.1 /etc/hosts
